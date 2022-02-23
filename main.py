66f5bedd8d7831c9c3105ca36d62e76d3db61592
sriragjayakumar <sriragjayakumar@gmail.com>
2022-02-23 13:42:29 +0000

    Added std distribution function

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ Created on Wed Feb 23 07:59:18 2022
 """
 
 import numpy as np
+
+def std_norm_dist_gen(k_arm):
+    
+    np.random.seed(7)
+    distribution = np.random.normal(loc=10,scale=1,size=k_arm)
+    
+    return distribution
