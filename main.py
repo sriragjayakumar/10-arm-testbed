f52d312b0d2a26eb3c7f68f6c48bd8bbe863a219
sriragjayakumar <sriragjayakumar@gmail.com>
2022-02-24 16:13:01 +0000

    plot for e=0

--- main.py
+++ main.py
@@ -19,14 +19,42 @@ def testbed_plot(distribution):
     plt.legend()
 
     
-def std_norm_dist_gen(k_arm):
+def std_norm_dist_gen(mean,std,k):
     
-    np.random.seed(7)
-    distribution = np.random.normal(loc=0,scale=1,size=k_arm)
+    # np.random.seed(7)
+    distribution = np.random.normal(mean,std,k)
     
     return distribution
 
 
 
 if __name__ == '__main__':
-    testbed_plot(std_norm_dist_gen(10))
+    
+    
+    for _ in range(2000):
+        
+        actArr = std_norm_dist_gen(0,1,10)
+        valEstimates = np.zeros(10)
+        kAction = np.zeros(10)         
+        rSum = np.zeros(10) 
+        scoreArr = np.zeros((1000, 1))
+        for step in range(1000):
+            
+            maxAction = np.argmax(valEstimates)     # Find max value estimate
+            # identify the corresponding action, as array containing only actions with max
+            action = np.where(valEstimates == np.argmax(valEstimates))[0]
+
+            # If multiple actions contain the same value, randomly select an action
+            if len(action) == 0:
+                actionT = maxAction
+            else:
+                actionT = np.random.choice(action)
+            reward = std_norm_dist_gen(actArr[actionT],1,1)[0]
+            
+            kAction[actionT] += 1       # Add 1 to action selection
+            rSum[actionT] += reward    # Add reward to sum array
+    
+            # Calculate new action-value, sum(r)/ka
+            valEstimates[actionT] = rSum[actionT]/kAction[actionT]
+            
+            scoreArr[step]=reward
